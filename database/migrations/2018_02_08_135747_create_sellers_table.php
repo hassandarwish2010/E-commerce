52b6c99b747173a25dea4bfcd253b341031fb8a9
antonious <atony.hosny@gmail.com>
2018-02-10 13:45:46 +0200

    sfsgf

---
+++ database/migrations/2018_02_08_135747_create_sellers_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSellersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sellers', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->string('phone');
+            $table->string('address');
+            $table->integer('comp_id')->unsigned();
+            $table->foreign('comp_id')->references('id')->on('companies')
+            ->onDelete('cascade')
+            ->onUpdate('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sellers');
+    }
+}
